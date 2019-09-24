8dd46dd3e53aa327c88c444b5fdc510204140516
Denis Shelest <axules@gmail.com>
2019-09-24 23:34:46 +0300

    update tests

--- src/tests/deepToMutate.test.js
+++ src/tests/deepToMutate.test.js
@@ -27,13 +27,32 @@ const data = [
 
   [
     { a: { a1: 1, a4: { a41: 1, a42: 5 } } },
-    [deepPatch({ a: { a4: { a41: 41 } } }), deepPatch({ a: { a4: { a42: 42 } } })],
-    { a: { a1: 1, a4: { a41: 41, a42: 42 } } }
+    [deepPatch({ a: { a4: { a41: 41 } } }), deepPatch({ a: { a4: { a42: [1, 2, 3] } } })],
+    { a: { a1: 1, a4: { a41: 41, a42: [1, 2, 3] } } }
+  ],
+
+  [
+    { a: [{ z: 100, z2: [1,2,3] }] },
+    deepPatch({ a: { '[0]': { z: 9, z2: ['q'] } } }),
+    { a: [{ z: 9, z2: ['q'] }] },
+  ],
+
+  [
+    { a: [1, 2] },
+    deepPatch({ a: { '[]': 3, '[+123]': 4 } }),
+    { a: [1, 2, 3, 4] },
   ],
 ];
 
 describe('mutate with deep', () => {
   test.each(data)('mutate(%j, %j) === %j', (obj, ext, expected) => {
     expect(mutate(obj, ext)).toEqual(expected);
+  });
+
+  test('not Objects should be as it is', () => {
+    const err = new Error('example');
+    const result = mutate({ a: 10 }, deepPatch({ a: err }));
+    expect(result).toEqual({ a: err });
+    expect(result.a).toBe(err);
   });
 });
