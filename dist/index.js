6a5ff7921a357f3ee64967d41b75268815c7fdd7
Denis Shelest <axules@gmail.com>
2024-07-29 16:49:47 -0400

    refactor: build

--- dist/index.js
+++ dist/index.js
@@ -13,26 +13,20 @@ exports.getValue = getValue;
 exports.isArrayElement = isArrayElement;
 exports.separatePath = separatePath;
 exports.splitPath = splitPath;
-
 function XMutateRemovedElementX() {}
-
 function XMutateLockedElementX(value) {
   this.__value__ = value;
 }
-
 function XDeepPatchX(value) {
   this.__value__ = value;
 }
-
 function XIssetMarkerX() {}
-
 var ARRAY_REGEXP = new RegExp('^\\[([^\\[\\]]*)\\]$');
 var MUTATE_TYPES = {
   ARRAY: 'array',
   OBJECT: 'object',
   DEFAULT: ''
 };
-
 function mutateObj(point, vType) {
   if (!point) return vType === MUTATE_TYPES.ARRAY ? [] : {};
   if (Array.isArray(point)) return [].concat(point);
@@ -41,29 +35,26 @@ function mutateObj(point, vType) {
   if (vType === MUTATE_TYPES.OBJECT) return {};
   return point;
 }
-
 function getObjectPaths(obj, prefix, map) {
   if (prefix === void 0) {
     prefix = [];
   }
-
   if (map === void 0) {
     map = null;
   }
-
   if (!checkIsNativeObject(obj)) return [];
   var keys = Object.keys(obj);
   var isRoot = !map;
-  var myMap = isRoot // eslint-disable-next-line no-undef
-  ? new Map() : map; // ignore objects that were listened. It means that recursive links will be ignored
+  var myMap = isRoot
+  // eslint-disable-next-line no-undef
+  ? new Map() : map;
 
+  // ignore objects that were listened. It means that recursive links will be ignored
   if (myMap.has(obj)) return null;
   myMap.set(obj, new XIssetMarkerX());
-
   for (var i = 0; i < keys.length; i++) {
     var value = obj[keys[i]];
     var currentPath = prefix.concat([keys[i]]);
-
     if (checkIsNativeObject(value)) {
       getObjectPaths(value, currentPath, myMap);
     } else if (checkIsDeepPatch(value)) {
@@ -75,7 +66,6 @@ function getObjectPaths(obj, prefix, map) {
       myMap.set(containsDot ? currentPath : currentPath.join('.'), value);
     }
   }
-
   if (isRoot) {
     var result = [];
     myMap.forEach(function (val, key) {
@@ -84,13 +74,10 @@ function getObjectPaths(obj, prefix, map) {
     });
     return result;
   }
-
   return null;
 }
-
 function extToArray(pExt) {
   var result = pExt;
-
   if (!Array.isArray(pExt)) {
     if (checkIsDeepPatch(pExt)) result = [pExt];else {
       if (!checkIsNativeObject(pExt)) return [];
@@ -100,12 +87,10 @@ function extToArray(pExt) {
       });
     }
   }
-
   return result.reduce(function (R, pair) {
     var isDeep = checkIsDeepPatch(pair);
     if (!isDeep && (!pair || pair.length < 2)) return R;
     var pairVal = isDeep ? pair : getPairValue(pair);
-
     if (isDeep || checkIsDeepPatch(pairVal)) {
       var pairPath = isDeep || !pair[0] ? undefined : splitPath(pair[0]);
       var n = R.findIndex(function (el) {
@@ -114,146 +99,115 @@ function extToArray(pExt) {
       if (n < 0) return R;
       return R.slice(0, n).concat(getObjectPaths(pairVal.__value__, pairPath), R.slice(n + 1));
     }
-
     return R;
   }, result);
 }
-
 function separatePath(path) {
   return typeof path == 'string' ? path.replace(new RegExp('([^\\.])(\\[)', 'g'), function (match, p1, p2) {
     return p1 + '.' + p2;
   }) : path;
 }
-
 function splitPath(path) {
   if (typeof path == 'string') return path.split('.');
   if (Array.isArray(path)) return path;
   throw new Error('Path should be String or Array of Strings');
 }
-
 function hasProperty(obj, prop) {
   return Object.prototype.hasOwnProperty.call(obj, prop);
 }
-
 function setValue(parent, key, value) {
   var isRemove = checkIsRemoved(value);
   var isUndefinedKey = checkIsUndefined(key);
-
   if (isRemove && (isUndefinedKey || !hasProperty(parent, key))) {
     return parent;
   }
-
   var isLocked = checkIsLocked(parent);
-
   if (isUndefinedKey) {
     if (isLocked) parent.__value__ = value;else return value;
   } else {
     var realParent = isLocked ? parent.__value__ : parent;
-
     if (isRemove) {
       if (Array.isArray(realParent)) realParent.splice(key, 1);else delete realParent[key];
     } else realParent[key] = value;
   }
-
   return parent;
 }
-
 function checkIsUndefined(value) {
   return typeof value === 'undefined';
 }
-
 function checkIsRemoved(pObj) {
   return pObj instanceof XMutateRemovedElementX;
 }
-
 function checkIsLocked(pObj) {
   return pObj instanceof XMutateLockedElementX;
 }
-
 function checkIsDeepPatch(value) {
   return value instanceof XDeepPatchX;
 }
-
 function checkIsObject(value) {
   return value && typeof value === 'object';
 }
-
 function checkIsNativeObject(value) {
   return checkIsObject(value) && value.__proto__.constructor.name === 'Object';
-} // It has been exported for tests, but you could use it if needed
-
-
+}
+// It has been exported for tests, but you could use it if needed
 function checkIsExists(pObject, pPath) {
   return !checkIsUndefined(getValue(pObject, pPath));
 }
-
 function getValue(pObject, pPath) {
   if (!checkIsObject(pObject)) return undefined;
   var pieces = splitPath(pPath);
-  if (pieces.length === 0) return pObject; // function preparePiece(piece) {
+  if (pieces.length === 0) return pObject;
+
+  // function preparePiece(piece) {
   //   return piece.replace(/(\[|\])+/g, '');
   // }
 
   var lastIndex = pieces.length - 1;
   var node = pObject;
-
   for (var i = 0; i < lastIndex; i++) {
     var piece = getRealIndex(node, pieces[i]);
     node = checkIsLocked(node[piece]) ? node[piece].__value__ : node[piece];
-
     if (!node || !checkIsObject(node) || checkIsRemoved(node)) {
       return undefined;
     }
   }
-
   return node[getRealIndex(node, pieces[lastIndex])];
 }
-
 function isArrayElement(key) {
   return ARRAY_REGEXP.test(key);
 }
-
 function getPairValue(pair) {
   if (!Array.isArray(pair) || pair.length === 0) return undefined;
   var valueIndex = pair.length - 1 || 1;
   return pair[valueIndex];
 }
-
 function extToTree(pExt, pSource) {
   // +++++++++++++++++++++++++++
   function getNewValue(pair, isMutated) {
     if (!pair || pair.length === 0) return undefined;
-
     if (pair.length === 1) {
       return new XMutateRemovedElementX();
     }
-
     var pairValue = getPairValue(pair);
-
     if (!isMutated && checkIsObject(pairValue)) {
       return new XMutateLockedElementX(pairValue);
     }
-
     if (checkIsUndefined(pairValue)) {
       return new XMutateRemovedElementX();
     }
-
     return pairValue;
-  } // +++++++++++++++++++++++++++
-
-
+  }
+  // +++++++++++++++++++++++++++
   if (!checkIsObject(pExt)) throw new Error('Changes should be Object or Array');
   var values = extToArray(pExt);
   return values.reduce(function (FULL_RESULT, PAIR) {
     if (!PAIR) return FULL_RESULT;
     if (typeof PAIR === 'string') PAIR = [PAIR];
-
     if (!PAIR[0] && PAIR[0] !== 0) {
       throw new Error('Path should not be empty');
     }
-
     var pathPieces = splitPath(separatePath(PAIR[0]));
-
     if (PAIR.length < 2 || checkIsUndefined(getPairValue(PAIR))) {
       if (!(checkIsExists(pSource, pathPieces) || checkIsExists(FULL_RESULT, pathPieces))) {
         return FULL_RESULT;
@@ -262,7 +216,6 @@ function extToTree(pExt, pSource) {
       var currentValue = getValue(pSource, pathPieces);
       if (currentValue === getPairValue(PAIR)) return FULL_RESULT;
     }
-
     var isLockedPath = false;
     pathPieces.reduce(function (parent, currentKey, currentI) {
       var isLastPiece = currentI >= pathPieces.length - 1;
@@ -270,15 +223,12 @@ function extToTree(pExt, pSource) {
       var newKey = isLockedPath ? getOptions(actualKey, parent).realKey : actualKey;
       var isLockedCurrent = !isLockedPath && hasProperty(parent, newKey) && checkIsLocked(parent[newKey]);
       isLockedPath = isLockedPath || isLockedCurrent;
-
       if (isLastPiece) {
         var newValue = getNewValue(PAIR, isLockedPath);
         if (isLockedPath) setValue(parent, newKey, newValue);else parent[newKey] = newValue;
         return FULL_RESULT;
       }
-
       var currentValue = isLockedCurrent ? parent[newKey].__value__ : parent[newKey];
-
       if (!checkIsObject(currentValue)) {
         // if (!!currentValue) {
         //   console.warn(`Warning: In "${PAIR[0]}", bad value for "${currentKey}", it will be replaced by empty Object ({})`);
@@ -287,13 +237,11 @@ function extToTree(pExt, pSource) {
         if (isLockedPath) setValue(parent, newKey, _newValue);else parent[newKey] = _newValue;
         return _newValue;
       }
-
       return currentValue;
     }, FULL_RESULT);
     return FULL_RESULT;
   }, {});
 }
-
 function updateSection(point, tree) {
   if (!tree || Array.isArray(tree) || !checkIsObject(tree)) return tree;
   if (checkIsLocked(tree)) return tree.__value__;
@@ -303,40 +251,35 @@ function updateSection(point, tree) {
   pieces.forEach(function (key) {
     var opt = getOptions(key, result);
     var k = opt.realKey;
-
     if (checkIsRemoved(tree[key])) {
-      if (opt.isArray) result.splice(k, 1);else delete result[k]; // setValue(result, k, tree[key]);
+      if (opt.isArray) result.splice(k, 1);else delete result[k];
+      // setValue(result, k, tree[key]);
     } else if (!opt.isArray || k >= 0) {
       result[k] = updateSection(result[k], tree[key]);
     }
   });
   return result;
 }
-
 function getRealIndex(items, key) {
   var parse = key ? ARRAY_REGEXP.exec(key) : null;
   if (!parse) return key;
   var arrayItems = Array.isArray(items) ? items : [];
   var k = parse[1].trim();
-
   if (k.length == 0 || k.slice(0, 1) == '+') {
     return arrayItems.length;
   } else if (k.slice(0, 1) == '=') {
     var parseCompare = /^=(?:([^=\s]*)=)?(.*)$/.exec(k);
-
     if (parseCompare) {
       var _k = parseCompare[1],
-          v = parseCompare[2];
+        v = parseCompare[2];
       return arrayItems.findIndex(function (el) {
         return String(_k && el ? getValue(el, _k) : el) === String(v);
       });
     }
   }
-
   var index = parseInt(k, 10);
   return Number.isNaN(index) ? items.length : index;
 }
-
 function getOptions(key, parentValue) {
   var realParentValue = checkIsLocked(parentValue) ? parentValue.__value__ : parentValue;
   var result = {
@@ -347,42 +290,32 @@ function getOptions(key, parentValue) {
   };
   return result;
 }
-
 function mutate(pObj, pExt) {
   if (!checkIsObject(pObj)) {
     throw new Error('Type of variable shoud be Object or Array');
   }
-
   if (checkIsUndefined(pExt)) {
     return toFunction(pObj);
   }
-
   var tree = extToTree(pExt, pObj);
-
   if (Object.getOwnPropertyNames(tree).length === 0) {
     return pObj;
   }
-
   return updateSection(pObj, tree);
 }
-
 function deepPatch(pExt) {
   if (checkIsDeepPatch(pExt)) return pExt;
   return new XDeepPatchX(pExt);
 }
-
 mutate.deep = function (pObj, pExt) {
   var newExt = null;
-
   if (Array.isArray(pExt)) {
     newExt = pExt.map(function (el) {
       return deepPatch(el);
     });
   } else newExt = deepPatch(pExt);
-
   return mutate(pObj, newExt);
 };
-
 function toFunction(pObj) {
   var result = pObj;
   return function (pExt) {
@@ -391,6 +324,4 @@ function toFunction(pObj) {
     return result;
   };
 }
-
-var _default = mutate;
-exports.default = _default;
+var _default = exports.default = mutate;
